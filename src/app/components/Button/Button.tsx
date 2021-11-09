5903378c58e7d77e5152cc877768b768069990f9
marwinburesch <marwin@posteo.de>
2021-11-09 15:43:10 +0100

    Refactor Button to utilise styled-components

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,12 +1,23 @@
 import type { ReactNode } from 'react';
 import React from 'react';
-import classes from './Button.module.css';
+import styled from 'styled-components';
 
 type ButtonProps = {
   children: ReactNode;
 };
+
 function Button({ children }: ButtonProps): JSX.Element {
-  return <button className={classes.button}>{children}</button>;
+  return <YellowButton>{children}</YellowButton>;
 }
 
 export default Button;
+
+const YellowButton = styled.button`
+  color: #000;
+  background: linear-gradient(180deg, #ffbb3b, #ffa90a);
+  font-size: 0.8em;
+  padding: 0.8em 2em;
+  border: none;
+  border-radius: 0.4em;
+  text-transform: uppercase;
+`;
