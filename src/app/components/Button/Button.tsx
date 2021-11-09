dedcdece661ed6a9ff4addd90b40fd61a3d9a5ef
marwinburesch <marwin@posteo.de>
2021-11-09 17:04:05 +0100

    Implement large prop
    to demonstrate conditional styles in styled components

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -4,18 +4,19 @@ import styled from 'styled-components';
 
 type ButtonProps = {
   children: ReactNode;
+  large?: boolean;
 };
 
-function Button({ children }: ButtonProps): JSX.Element {
-  return <StyledButton>{children}</StyledButton>;
+function Button({ children, large }: ButtonProps): JSX.Element {
+  return <StyledButton large={large}>{children}</StyledButton>;
 }
 
 export default Button;
 
-const StyledButton = styled.button`
+const StyledButton = styled.button<Partial<ButtonProps>>`
   color: #000;
   background: linear-gradient(180deg, #ffbb3b, #ffa90a);
-  font-size: 0.8em;
+  font-size: ${(props) => (props.large ? '1.8em' : '0.8em')};
   padding: 0.8em 2em;
   border: none;
   border-radius: 0.4em;
