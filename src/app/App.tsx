5378c0681763d7273a1b6fed2afcdbf5d1300748
marwinburesch <marwin@posteo.de>
2021-11-10 14:53:33 +0100

    Render Dashboard page in App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard/Dashboard';
 import Stuff from './pages/Stuff/Stuff';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<p>Main</p>} />
+        <Route path="/" element={<Dashboard />} />
         <Route path="stuff">
           <Route path=":thing" element={<Stuff />} />
         </Route>
