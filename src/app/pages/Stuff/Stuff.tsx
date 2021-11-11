1323f6bb1dde3d87d9391d1de89b3f97641cb157
phlgr <philipp@gartz.dev>
2021-11-11 12:07:52 +0100

    ♻️ Add DetailCard to Stuff Page

--- src/app/pages/Stuff/Stuff.tsx
+++ src/app/pages/Stuff/Stuff.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router';
+import DetailCard from '../../components/DetailCard/DetailCard';
 
 export default function Stuff(): JSX.Element {
   const { thing } = useParams();
-  return <p>Stuff: {thing}</p>;
+
+  return (
+    <DetailCard
+      name="Old Toothbrush"
+      description="You could use it to clean shoes"
+      categories={['toiletry', 'tool', 'every day use', 'plastic']}
+    />
+  );
 }
