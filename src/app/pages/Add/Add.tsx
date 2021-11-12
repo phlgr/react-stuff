857dc2f111c60db0729461023512063b4251d4b8
phlgr <philipp@gartz.dev>
2021-11-12 16:57:14 +0100

    ✨ Control input elements

--- src/app/pages/Add/Add.tsx
+++ src/app/pages/Add/Add.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Add(): JSX.Element {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
   return (
     <form>
-      <input type="text" placeholder="Name" />
-      <input type="text" placeholder="Description" />
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Description"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
       <input type="submit" />
     </form>
   );
