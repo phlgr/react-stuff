3a76537be16045e35ec5de07899e11a767703e5f
phlgr <philipp@gartz.dev>
2021-11-15 11:40:24 +0100

    ✨ Create handleDeleteClick function

--- src/app/pages/Stuff/Stuff.tsx
+++ src/app/pages/Stuff/Stuff.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 import DetailCard from '../../components/DetailCard/DetailCard';
 import useFetch from '../../hooks/useFetch';
 import type { Thing } from '../../types';
 
 export default function Stuff(): JSX.Element {
+  const navigate = useNavigate();
   const { thingID } = useParams();
   const thing = useFetch<Thing>(
     `https://json-server.neuefische.de/stuff/${thingID}`
   );
+
+  async function handleDeleteClick(): Promise<void> {
+    await fetch(`https://json-server.neuefische.de/stuff/${thingID}`, {
+      method: 'DELETE',
+    });
+    navigate('/');
+  }
 
   return (
     <>
@@ -17,6 +25,7 @@ export default function Stuff(): JSX.Element {
           name={thing.name}
           description={thing.description}
           categories={thing.categories}
+          onDelete={handleDeleteClick}
         />
       )}
     </>
